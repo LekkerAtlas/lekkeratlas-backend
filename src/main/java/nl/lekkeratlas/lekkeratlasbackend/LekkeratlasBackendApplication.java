92bf180b3ac27c281c420971ee987dc922c2ae5f
david <david@aukes.com>
2026-03-31 15:07:19 +0200

    added initial structure

---
+++ src/main/java/nl/lekkeratlas/lekkeratlasbackend/LekkeratlasBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.lekkeratlas.lekkeratlasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LekkeratlasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LekkeratlasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/lekkeratlas/lekkeratlasbackend/LekkeratlasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.lekkeratlas.lekkeratlasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LekkeratlasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
