a5eef2585c9328e6832bf0eaae8dc660ae2fa67d
david <david@aukes.com>
2026-04-23 01:33:16 +0200

    fixed tests

--- src/test/java/nl/lekkeratlas/lekkeratlasbackend/LekkeratlasBackendApplicationTests.java
+++ src/test/java/nl/lekkeratlas/lekkeratlasbackend/LekkeratlasBackendApplicationTests.java
@@ -4,10 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
-		"AUTHENTIK_HOST=http://localhost:9000",
-		"AUTHENTIK_CLIENT_ID=test-client-id",
-		"AUTHENTIK_CLIENT_SECRET=test-client-secret",
-		"spring.http.clients.imperative.factory=simple" // Is this needed?
+        "AUTHENTIK_HOST=http://localhost:9000",
+        "AUTHENTIK_CLIENT_ID=test-client-id",
+        "AUTHENTIK_CLIENT_SECRET=test-client-secret",
+        "spring.security.oauth2.client.provider.authentik.authorization-uri=http://localhost:9000/application/o/authorize/",
+        "spring.security.oauth2.client.provider.authentik.token-uri=http://localhost:9000/application/o/token/",
+        "spring.security.oauth2.client.provider.authentik.user-info-uri=http://localhost:9000/application/o/userinfo/",
+        "spring.security.oauth2.client.provider.authentik.user-name-attribute=sub",
+        "spring.security.oauth2.client.provider.authentik.jwk-set-uri=http://localhost:9000/application/o/lekker-atlas/jwks/",
+        "spring.security.oauth2.resourceserver.jwt.jwk-set-uri=http://localhost:9000/application/o/lekker-atlas/jwks/"
 })
 class LekkeratlasBackendApplicationTests {
 
