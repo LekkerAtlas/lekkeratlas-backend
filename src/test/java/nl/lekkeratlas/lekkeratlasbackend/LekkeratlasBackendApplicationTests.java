d33bdf6ee6d336e727622f44554b6e8b806b36bd
david <david@aukes.com>
2026-04-23 01:29:32 +0200

    fixed tests

--- src/test/java/nl/lekkeratlas/lekkeratlasbackend/LekkeratlasBackendApplicationTests.java
+++ src/test/java/nl/lekkeratlas/lekkeratlasbackend/LekkeratlasBackendApplicationTests.java
@@ -3,11 +3,15 @@ package nl.lekkeratlas.lekkeratlasbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"AUTHENTIK_HOST=http://localhost:9000",
+		"AUTHENTIK_CLIENT_ID=test-client-id",
+		"AUTHENTIK_CLIENT_SECRET=test-client-secret",
+		"spring.http.clients.imperative.factory=simple" // Is this needed?
+})
 class LekkeratlasBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
