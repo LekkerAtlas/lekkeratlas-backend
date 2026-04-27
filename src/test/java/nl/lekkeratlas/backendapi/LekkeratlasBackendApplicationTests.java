33a6aa5965d2603be0ad201306ef5f543b686ac4
david <david@aukes.com>
2026-04-27 22:53:50 +0200

    updated propperties

--- src/test/java/nl/lekkeratlas/backendapi/LekkeratlasBackendApplicationTests.java
+++ src/test/java/nl/lekkeratlas/backendapi/LekkeratlasBackendApplicationTests.java
@@ -2,14 +2,18 @@ package nl.lekkeratlas.backendapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(
         classes = LekkeratlasBackendApplication.class,
         properties = {
-                "spring.profiles.active=backend,dev,test",
+                "spring.autoconfigure.exclude=" +
+                        "org.springframework.boot.security.oauth2.client.autoconfigure.OAuth2ClientAutoConfiguration," +
+                        "org.springframework.boot.security.oauth2.server.resource.autoconfigure.servlet.OAuth2ResourceServerAutoConfiguration",
                 "spring.rabbitmq.listener.simple.auto-startup=false"
         }
 )
+@ActiveProfiles("backend")
 class LekkeratlasBackendApplicationTests {
 
     @Test
--- src/test/java/nl/lekkeratlas/worker/WorkerApplicationTests.java
+++ src/test/java/nl/lekkeratlas/worker/WorkerApplicationTests.java
@@ -2,15 +2,19 @@ package nl.lekkeratlas.worker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(
         classes = WorkerApplication.class,
         properties = {
-                "spring.profiles.active=worker,dev,test",
-                "spring.main.web-application-type=none",
-                "spring.rabbitmq.listener.simple.auto-startup=false"
+                "spring.autoconfigure.exclude=" +
+                        "org.springframework.boot.security.oauth2.client.autoconfigure.OAuth2ClientAutoConfiguration," +
+                        "org.springframework.boot.security.oauth2.server.resource.autoconfigure.servlet.OAuth2ResourceServerAutoConfiguration",
+                "spring.rabbitmq.listener.simple.auto-startup=false",
+                "spring.main.web-application-type=none"
         }
 )
+@ActiveProfiles("worker")
 class WorkerApplicationTests {
 
     @Test
